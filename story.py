9f7484bacf0a73b667174f45bff7cd58b02dfb9a
Josue Garcia <josuegarciav24@gmail.com>
2026-08-13 15:39:36 +0000

    hi from the other side

--- story.py
+++ story.py
@@ -14,16 +14,18 @@ def main():
     # print("hello", end=" ")
     # print(planet)
 
-    name = input("What is your name? ")
-    color = input("Pick a color brodie: ")
-    adj = input("Gimme an adjective: ")
-    Goal = input("What is your life´s goal? ")
+    name = input("What is your name? ").strip().title()
+    color = input("Pick a color brodie: ").strip().lower()
+    adj = input("Gimme an adjective: ").strip().lower()
+    Goal = input("What is your life´s goal? \n").strip().lower()
 
     print(f"Hello, {name}\n\n")
 
     print("This is your story:")
     print(f"At dawn the sky turned {color}, and the\n air felt {adj}. I decided today I will finally\n {Goal}.")
 
+    yelling = f"At dawn the sky turned {color}, and the\n air felt {adj}. I decided today I will finally {Goal}.\n"
+    print(yelling.upper())
 
 
 if __name__=="__main__":
