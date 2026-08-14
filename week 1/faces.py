de0c17bf8c4f058f19ad4e2f1b808b0a3efb3846
Josue Garcia <josuegarciav24@gmail.com>
2026-08-14 15:35:38 +0000

    finally works bretheren

--- week 1/faces.py
+++ week 1/faces.py
@@ -4,7 +4,7 @@ def main():
     masterpiece = input(" ")
 
     masterpiece = masterpiece.replace(":)","🙂", count=-1)
-    masterpiece = masterpiece.replace(":)",":(🙁", count=-1)
+    masterpiece = masterpiece.replace(":(","🙁", count=-1)
     print(f"{masterpiece}")
 
 if __name__ == "__main__":
