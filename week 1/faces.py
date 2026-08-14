420262ffed0bf6cd3d940971f02c4762a4632ef4
Josue Garcia <josuegarciav24@gmail.com>
2026-08-14 15:32:32 +0000

    hsrhfh

--- week 1/faces.py
+++ week 1/faces.py
@@ -5,7 +5,7 @@ def main():
 
     masterpiece = masterpiece.replace(":)","🙂", count=-1)
     masterpiece = masterpiece.replace(":)",":(🙁", count=-1)
-    print(f"Hello {masterpiece}")
+    print(f"{masterpiece}")
 
 if __name__ == "__main__":
     main()
