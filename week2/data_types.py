7d3dd063ed22b571411b3b30f9777ea0a15b4090
Josue Garcia <josuegarciav24@gmail.com>
2026-08-17 14:55:19 +0000

    rectangle width

---
+++ week2/data_types.py
@@ -0,0 +1,15 @@
+
+def main():
+
+    F = int(input("Rectangle width: "))
+
+    h = int(F) * "0"
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+
+
+if __name__ == "__main__":
+    main()
