747564f7f16501f88d66511361b51cb3f403af0f
Josue Garcia <josuegarciav24@gmail.com>
2026-08-24 15:00:28 +0000

    fsa

--- week 3/password.py
+++ week 3/password.py
@@ -1,17 +1,26 @@
+import time
 
 def main():
 
+    x = 9
     password = "password"
     ppword = input("enter the password: ")
+
 
     if ppword == password:
         print("you have succesfully entered Epstein island")
 
     else:
-       print("Incorrect, buenas noches pequeño")
+       print("Incorrect, thanks for trying -Mr. Epstein")
 
 
-    print("this program will self destruct in 5 seconds")
+    print("this program will self destruct in 10 seconds.")
+    for i in range(9):
+        time.sleep(1)
+        print(x)
+        x = x - 1
+    time.sleep(2)
+    print("SYSTEM DELETED")
 
 
 
