b7631ed3f174545fe447fd799c4730e6dc4c722c
Josue Garcia <josuegarciav24@gmail.com>
2026-08-24 14:46:50 +0000

    epstein island

---
+++ week 3/password.py
@@ -0,0 +1,20 @@
+
+def main():
+
+    password = "password"
+    ppword = input("enter the password: ")
+
+    if ppword == password:
+        print("you have succesfully entered Epstein island")
+
+    else:
+       print("buenas noches pequeño")
+
+
+    print("this program will self destruct in 5 seconds")
+
+
+
+if __name__ == "__main__":
+    main()
+
