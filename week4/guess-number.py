d3d4f014705c30069b69b6d53016cd122e496521
Josue Garcia <josuegarciav24@gmail.com>
2026-09-03 15:26:48 +0000

    soy un exitoso(guess number)

--- week4/guess-number.py
+++ week4/guess-number.py
@@ -8,17 +8,61 @@ def main():
     dif = int(input("Choose a dificulty level:[1]Easy, [2]Medium, [3]Hard "))
 
     if dif == 1:
-        numb = random.randit(1,20)
+        numb = random.randint(1,20)
         print("I'm thinking of a number between 1-20")
         guess = int(input("Take a guess: "))
-            while guess != numb:
-                if guess < numb:
-                    print("too low")
-                    guess = int(input("Take a guess: "))
-                elif guess > numb:
-                    print("too high")
-                    guess = int(input("Take a guess: "))
-    print(f"congrats {nombre}, You guessed the number!")
+        att = 5
+        while att > 0:
+            if guess < numb:
+                print("too low")
+                att -= 1
+                print(f"Remaining attempts:{att}")
+                guess = int(input("Take a guess: "))
+            elif guess > numb:
+                print("too high")
+                att -= 1
+                guess = int(input("Take a guess: "))
+            elif guess == numb:
+                print(f"congrats {nombre}, You guessed the number!")
+                break
+
+    elif dif == 2:
+        numb = random.randint(1,50)
+        print("I'm thinking of a number between 1-50")
+        guess = int(input("Take a guess: "))
+        att = 7
+        while att > 0:
+            if guess < numb:
+                print("too low")
+                att -= 1
+                print(f"Remaining attempts:{att}")
+                guess = int(input("Take a guess: "))
+            elif guess > numb:
+                print("too high")
+                att -= 1
+                guess = int(input("Take a guess: "))
+            elif guess == numb:
+                print(f"congrats {nombre}, You guessed the number!")
+                break
+
+    elif dif == 3:
+        numb = random.randint(1,100)
+        print("I'm thinking of a number between 1-100")
+        guess = int(input("Take a guess: "))
+        att = 10
+        while att > 0:
+            if guess < numb:
+                print("too low")
+                att -= 1
+                print(f"Remaining attempts:{att}")
+                guess = int(input("Take a guess: "))
+            elif guess > numb:
+                print("too high")
+                att -= 1
+                guess = int(input("Take a guess: "))
+            elif guess == numb:
+                print(f"congrats {nombre}, You guessed the number!")
+                break
 
 
 
