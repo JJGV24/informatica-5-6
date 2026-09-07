fb911cf7fd5f83291a253a6583db373e0055e5c3
Josue Garcia <josuegarciav24@gmail.com>
2026-09-07 14:57:05 +0000

    project

---
+++ endtermproject.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+def main():
+
+    print("Lo otro eran simulacros.Tú eres el incendio.")
+    name = input("Cual es su nombre? ")
+    print(f"{name}, que bello nombre")
+    
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
