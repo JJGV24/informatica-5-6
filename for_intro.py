38ed85aa246c09b9f653312676f80eb4de861152
Josue Garcia <josuegarciav24@gmail.com>
2026-09-18 20:18:50 +0000

    for

---
+++ for_intro.py
@@ -0,0 +1,16 @@
+
+
+def main():
+
+    doctrine = ["Baptism", "Confirmation", "Holy Ghost", "Repentence", "Faith", "Endure to the End"]
+    num = 1
+
+    for i in doctrine:
+        print(f"{num}. {i}")
+        num += 1
+
+
+
+
+if __name__=="__main__":
+    main()
